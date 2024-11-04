848fb2d33dd3da77c831157f0c68515a53dc64bd
Isaac Northrop <isaac.northrop88@gmail.com>
2024-11-04 12:06:41 -0800

    this parsing is getting insane

--- html_parse_example.cpp
+++ html_parse_example.cpp
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fstream>
+#include <sstream>
 
 #include <myhtml/api.h>
 #include <iostream>
 
+mystatus_t serialization_callback(const char* data, size_t len, void* ctx)
+{
+    printf("%.*s", (int)len, data);
+    return MyCORE_STATUS_OK;
+}
+
 int main(int argc, const char * argv[])
 {
 
-    FILE* f = fopen("output.txt", "r");
-    char* buffer;
-    char* html;
-    int MAX_LENGTH = 100;
+    std::ifstream file("output.txt");
+    if(!file) {
+        std::cerr << "File could not be opened!" << std::endl;
+        return 1;
+    }
 
-    while(fgets(buffer, MAX_LENGTH, f))
-        strcat(html, buffer);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
 
-    std::cout << html << std::endl;
+    std::string content = buffer.str();
 
-    fclose(f);
+    const char* html = content.c_str();
 
-    
     // basic init
     myhtml_t* myhtml = myhtml_create();
     myhtml_init(myhtml, MyHTML_OPTIONS_DEFAULT, 1, 0);
@@ -32,6 +39,13 @@ int main(int argc, const char * argv[])
     
     // parse html
     myhtml_parse(tree, MyENCODING_UTF_8, html, strlen(html));
+
+    char* attr_key = "matchup-link";
+    myhtml_collection_t *collection = myhtml_get_nodes_by_attribute_key(tree, NULL, NULL, attr_key, strlen(attr_key), NULL);
+
+    for(size_t i = 0; i<collection->length; i++){
+        myhtml_serialization_node_callback(collection->list[i], serialization_callback, NULL);
+    }
     
     return 0;
 }
